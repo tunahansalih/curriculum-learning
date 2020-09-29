a5501c760c7bc0028185bff81acff61175df913f
Tuna Han Salih Meral <ths.meral@vispera.co>
2020-09-29 18:57:38 +0300

    adds curriculum sequence

---
+++ dataset/cifar100_curriculum.py
@@ -0,0 +1,51 @@
+import math
+import numpy as np
+from skimage.transform import resize
+
+import tensorflow as tf
+
+from dataset.cifar100 import CIFAR100
+
+class CIFAR100CurriculumSequence(tf.keras.utils.Sequence):
+    def __init__(self, x_set, y_set, ordered_indices, batch_size, step_length, increase, starting_percent, image_size=(299, 299)):
+        self.x, self.y = x_set, np.array(y_set)
+        self.ordered_indices = ordered_indices
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.step_length = step_length
+        self.increase = increase
+        self.starting_percent = starting_percent
+        
+
+    def __len__(self):
+        return math.ceil(len(self.x) / self.batch_size)
+
+    def preprocess_image(self, x):
+        x = x.reshape(3, 32, 32)
+        x = x.transpose(1, 2, 0)
+        x = x / 255.0
+        x = x - 0.5
+        x = x * 2.0
+        return x
+
+    def __getitem__(self, idx):
+        g = int(min(self.starting_percent*(self.increase **
+                                           math.floor(idx/self.step_length)), 1)*len(self.x))
+        fraction_indices = self.ordered_indices[:g].tolist()
+        fraction_x = self.x[fraction_indices]
+        fraction_y = self.y[fraction_indices]
+
+        batch_indices = np.arange(idx * self.batch_size, (idx + 1) * self.batch_size) % g
+        batch_x = fraction_x[batch_indices]
+        batch_y = fraction_y[batch_indices]
+        return (np.array([resize(self.preprocess_image(image), self.image_size) for image in batch_x]), batch_y)
+
+
+class CIFAR100Curriculum(CIFAR100):
+
+    @staticmethod
+    def load_generator(x, y, ordered_indices, batch_size, step_length, increase, starting_percent, image_size=(299, 299)):
+        cifar100_sequence = CIFAR100CurriculumSequence(x, y, ordered_indices, batch_size, step_length, increase, starting_percent, image_size)
+        enqueuer = tf.keras.utils.OrderedEnqueuer(cifar100_sequence, use_multiprocessing=False, shuffle=False)
+        enqueuer.start(workers=1, max_queue_size=10)
+        return enqueuer.get()
--- main.py
+++ main.py
@@ -11,86 +11,109 @@ from sklearn.svm import SVC
 from tqdm import tqdm
 
 from dataset import cifar100
+from dataset import cifar100_curriculum
 
 batch_size = 10
 result_csv_path = "data/cifar100_inception_features.csv"
 ordered_balanced_result_csv_path = "data/cifar100_ordered_balanced_indices.csv"
 svm_path = "data/svm.joblib"
 
-if not os.path.exists(result_csv_path):
-    (train_x, train_y), (test_x, test_y) = cifar100.CIFAR100.load_data()
-    num_batches_per_epoch = int(math.ceil(len(train_x) / batch_size))
-    generator = cifar100.CIFAR100.load_generator(
-        train_x, train_y, batch_size, image_size=(299, 299)
-    )
-    scoringModel = tf.keras.applications.InceptionV3(
-        include_top=False, weights="imagenet", pooling="avg"
-    )
-
-    with open(result_csv_path, "w", newline="") as write_obj:
-        column_names = [
-            "numpy_index",
-            "label",
-        ] + [f"feature_{i}" for i in range(scoringModel.output_shape[1])]
-        csv_writer = csv.DictWriter(
-            write_obj,
-            fieldnames=column_names,
+if not os.path.exists(ordered_balanced_result_csv_path):
+    if not os.path.exists(result_csv_path):
+        (train_x, train_y), (test_x, test_y) = cifar100.CIFAR100.load_data()
+        num_batches_per_epoch = int(math.ceil(len(train_x) / batch_size))
+        generator = cifar100.CIFAR100.load_generator(
+            train_x, train_y, batch_size, image_size=(299, 299)
         )
-        csv_writer.writeheader()
-        for i in tqdm(range(num_batches_per_epoch)):
-            (x, y) = next(generator)
-            results = scoringModel(x)
+        scoringModel = tf.keras.applications.InceptionV3(
+            include_top=False, weights="imagenet", pooling="avg"
+        )
 
-            for j, (feature, label) in enumerate(zip(results, y)):
-                feature_dict = {
-                    f"feature_{k}": f for k, f in enumerate(feature.numpy())
-                }
-                feature_dict["numpy_index"] = j + i * batch_size
-                feature_dict["label"] = label
+        with open(result_csv_path, "w", newline="") as write_obj:
+            column_names = [
+                "numpy_index",
+                "label",
+            ] + [f"feature_{i}" for i in range(scoringModel.output_shape[1])]
+            csv_writer = csv.DictWriter(
+                write_obj,
+                fieldnames=column_names,
+            )
+            csv_writer.writeheader()
+            for i in tqdm(range(num_batches_per_epoch)):
+                (x, y) = next(generator)
+                results = scoringModel(x)
 
-                csv_writer.writerow(feature_dict)
+                for j, (feature, label) in enumerate(zip(results, y)):
+                    feature_dict = {
+                        f"feature_{k}": f for k, f in enumerate(feature.numpy())
+                    }
+                    feature_dict["numpy_index"] = j + i * batch_size
+                    feature_dict["label"] = label
 
-
-df = pd.read_csv(result_csv_path)
-if "score" not in df.columns:
-    feature_columns = [column for column in df.columns if "feature" in column]
-    X = df[feature_columns].values
-    y = df["label"].values
-    if not os.path.exists(svm_path):
-        svc = SVC(probability=True)
-        svc.fit(X, y)
-        dump(svc, svm_path)
-    else:
-        svc = load(svm_path)
-
-    X = df[feature_columns].values
-    scores = []
-    for batch in tqdm(np.array_split(X, 100)):
-        scores_batch = svc.predict_proba(batch)
-        scores_batch = np.max(scores_batch, axis=-1)
-        scores.extend(scores_batch)
-
-    df["score"] = scores
-
-    df.to_csv(result_csv_path)
+                    csv_writer.writerow(feature_dict)
 
 
-labels = df["label"].values
-label_set, label_counts = np.unique(labels, return_counts=True)
+    df = pd.read_csv(result_csv_path)
+    if "score" not in df.columns:
+        feature_columns = [column for column in df.columns if "feature" in column]
+        X = df[feature_columns].values
+        y = df["label"].values
+        if not os.path.exists(svm_path):
+            svc = SVC(probability=True)
+            svc.fit(X, y)
+            dump(svc, svm_path)
+        else:
+            svc = load(svm_path)
 
-df_list = []
-for label in label_set:
-    df_label = df[df["label"] == label]
-    df_label = df_label.sort_values(by="score", ascending=False)
-    df_list.append(df_label)
+        X = df[feature_columns].values
+        scores = []
+        for batch in tqdm(np.array_split(X, 100)):
+            scores_batch = svc.predict_proba(batch)
+            scores_batch = np.max(scores_batch, axis=-1)
+            scores.extend(scores_batch)
 
-rows = []
-for i in range(min(label_counts)):
-    for j, label in enumerate(label_set):
-        rows.append(df_list[j].iloc[i])
+        df["score"] = scores
 
-ordered_balanced_df = pd.DataFrame(rows, columns=["numpy_index", "label", "score"])
-ordered_balanced_df = ordered_balanced_df.astype(
-    dtype={"numpy_index": "int64", "label": "int64", "score": "float64"}
+        df.to_csv(result_csv_path)
+
+
+    labels = df["label"].values
+    label_set, label_counts = np.unique(labels, return_counts=True)
+
+    df_list = []
+    for label in label_set:
+        df_label = df[df["label"] == label]
+        df_label = df_label.sort_values(by="score", ascending=False)
+        df_list.append(df_label)
+
+    rows = []
+    for i in range(min(label_counts)):
+        for j, label in enumerate(label_set):
+            rows.append(df_list[j].iloc[i])
+
+    ordered_balanced_df = pd.DataFrame(
+        rows, columns=["numpy_index", "label", "score"])
+    ordered_balanced_df = ordered_balanced_df.astype(
+        dtype={"numpy_index": "int64", "label": "int64", "score": "float64"}
+    )
+    ordered_balanced_df = ordered_balanced_df.reset_index(drop=True)
+    ordered_balanced_df.to_csv(ordered_balanced_result_csv_path)
+else:
+    ordered_balanced_df = pd.read_csv(ordered_balanced_result_csv_path)
+    ordered_balanced_df = ordered_balanced_df.astype(
+        dtype={"numpy_index": "int64", "label": "int64", "score": "float64"}
+    )
+
+
+ordered_indices = ordered_balanced_df["numpy_index"].values
+(train_x, train_y), (test_x, test_y) = cifar100_curriculum.CIFAR100Curriculum.load_data()
+num_steps = 10000
+
+generator = cifar100_curriculum.CIFAR100Curriculum.load_generator(
+    train_x, train_y, ordered_indices, batch_size=10, step_length=1000, increase=2, starting_percent=0.1, image_size=(299, 299)
 )
-ordered_balanced_df.to_csv(ordered_balanced_result_csv_path)
+
+for i in tqdm(range(num_steps)):
+    (x, y) = next(generator)
+
+    # TODO training script
